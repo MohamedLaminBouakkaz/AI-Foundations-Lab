fa735100c35699f5918805f52756e32ccb47750b
Mohamed Lamine <mohamed.lamine.bouakkaz@gmail.com>
2026-03-24 18:41:12 +0100

    feat: add plot learning curve function

--- utils.py
+++ utils.py
@@ -1,3 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 MSE = lambda y, y_pred: np.mean((y - y_pred) ** 2)
+
+
+def plot_learning_curve(cost):
+    plt.plot(cost)
+    plt.xlabel("epochs")
+    plt.ylabel("cost")
+    plt.legend()
+    plt.show()
