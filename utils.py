06216a50eb0af50735980e852d941dad8b652079
Mohamed Lamine <mohamed.lamine.bouakkaz@gmail.com>
2026-03-25 15:15:17 +0100

    fix: include labels in plot function to enable legend functionality

--- utils.py
+++ utils.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 MSE = lambda y, y_pred: np.mean((y - y_pred) ** 2)
 
 
-def plot_learning_curve(cost):
-    plt.plot(cost)
+def plot_learning_curve(cost, label):
+    plt.plot(cost, label=label)
     plt.xlabel("epochs")
     plt.ylabel("cost")
     plt.legend()
